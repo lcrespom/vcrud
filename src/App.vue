69a93eb8833168394e36a7c48096aeb91f628ecd
Luis Crespo <lcrespom@gmail.com>
2018-11-08 19:59:00 +0100

    Using bootstrap-vue

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,15 @@
 <template>
 	<div id="app">
-		<nav class="navbar navbar-expand-lg navbar-light bg-light">
-			<!-- Title -->
-			<a class="navbar-brand" href="javascript:void(0)">vCRUD</a>
-			<!-- Responsive hamburger menu -->
-			<button class="navbar-toggler" type="button" data-toggle="collapse"
-				data-target="#navbarSupportedContent"
-				aria-controls="navbarSupportedContent" aria-expanded="false"
-				aria-label="Toggle navigation">
-				<span class="navbar-toggler-icon"></span>
-			</button>
-			<!-- Collapsible nav elements -->
-			<div class="collapse navbar-collapse" id="navbarSupportedContent">
-				<ul class="navbar-nav mr-auto">
-					<li class="nav-item active">
-						<router-link to="/" class="nav-link">Home</router-link>
-					</li>
-					<li class="nav-item">
-						<router-link to="/about" class="nav-link">About</router-link>
-					</li>
-				</ul>
-			</div>
-		</nav>
+		<b-navbar toggleable="md" type="dark" variant="info">
+			<b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+			<b-navbar-brand href="javascript:void(0)">vCRUD</b-navbar-brand>
+			<b-collapse is-nav id="nav_collapse">
+				<b-navbar-nav>
+					<b-nav-item to="/">Home</b-nav-item>
+					<b-nav-item to="/about">About</b-nav-item>
+				</b-navbar-nav>
+			</b-collapse>
+		</b-navbar>
 
 		<router-view/>
 	</div>
