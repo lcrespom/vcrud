d14fc1abef69779a642b177972cca1add7bf0854
Luis Crespo <lcrespom@gmail.com>
2018-11-08 19:22:06 +0100

    Using bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,29 @@
 <template>
 	<div id="app">
-		<div id="nav">
-			<router-link to="/">Home</router-link>|
-			<router-link to="/about">About</router-link>
-		</div>
+		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+			<!-- Title -->
+			<a class="navbar-brand" href="javascript:void(0)">vCRUD</a>
+			<!-- Responsive hamburger menu -->
+			<button class="navbar-toggler" type="button" data-toggle="collapse"
+				data-target="#navbarSupportedContent"
+				aria-controls="navbarSupportedContent" aria-expanded="false"
+				aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<!-- Collapsible nav elements -->
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav mr-auto">
+					<li class="nav-item active">
+						<router-link to="/" class="nav-link">Home</router-link>
+					</li>
+					<li class="nav-item">
+						<router-link to="/about" class="nav-link">About</router-link>
+					</li>
+				</ul>
+			</div>
+		</nav>
+
 		<router-view/>
 	</div>
 </template>
 
-<style>
-#app {
-	font-family: "Avenir", Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
-}
-#nav {
-	padding: 30px;
-}
-
-#nav a {
-	font-weight: bold;
-	color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-	color: #42b983;
-}
-</style>
